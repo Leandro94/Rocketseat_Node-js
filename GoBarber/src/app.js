51820448523f02b6e8c651c1a108a3ad40c00bef
LeandroR94 <reisleandro868@gmail.com>
2019-09-01 13:37:48 -0300

    ESLint, Prettier & EditorConfig

--- GoBarber/src/app.js
+++ GoBarber/src/app.js
@@ -1,14 +1,12 @@
-//importacoes antes do sucrase:
-//const express = require("express");
-//const routes = require("./routes");
-
-import express from "express";
-import routes from "./routes";
+// importacoes antes do sucrase:
+// const express = require("express");
+// const routes = require("./routes");
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-
     this.middlewares();
     this.routes();
   }
@@ -16,9 +14,10 @@ class App {
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
 }
-//module.exports = new App().server; ants do sucrase
+// module.exports = new App().server; ants do sucrase
 export default new App().server;
--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello Word!" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Word!' });
 });
-//module.exports = routes;
+// module.exports = routes;
 export default routes;
