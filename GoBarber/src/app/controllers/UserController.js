4db33f42d542fe24e3b00184d766f080ce2913c7
LeandroR94 <reisleandro868@gmail.com>
2019-09-05 10:17:19 -0300

    Update do usuário

--- GoBarber/src/app/controllers/UserController.js
+++ GoBarber/src/app/controllers/UserController.js
@@ -18,8 +18,25 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    if (email != user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ erro: 'User already exits.' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, nome, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      nome,
+      email,
+      provider,
+    });
   }
 }
 export default new UserController();
