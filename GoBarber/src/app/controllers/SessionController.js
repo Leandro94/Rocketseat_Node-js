f65e4ff584fd2861160b40d0f66aea214dab4a2f
LeandroR94 <reisleandro868@gmail.com>
2019-11-04 11:37:33 -0300

    Add provider do user

--- GoBarber/src/app/controllers/SessionController.js
+++ GoBarber/src/app/controllers/SessionController.js
@@ -33,12 +33,13 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
-    const { id, nome, avatar } = user;
+    const { id, nome, avatar, provider } = user;
     return res.json({
       user: {
         id,
         nome,
         email,
+        provider,
         avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
